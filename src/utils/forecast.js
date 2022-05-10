9f1a749fb264ec659edb9e3025cc128b0b1f7564
liam <minthantnyi1212@gmail.com>
2022-05-10 11:59:16 +0630

    a few changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,14 @@
 const request = require('request');
 
 const forecast = (lat, lng, callback) => {
-    const url = 'https://api.ambeedata.com/weather/forecast/by-lat-lng?lat='+ encodeURI(lat) + '&lng=' + encodeURI(lng) + '&filter=minutely&units=si&x-api-key=c1fc8134031f69982faef7f4c20749aec4fb98cdc3877d09aa0167c39da6917b'
+    const url = 'https://api.ambeedata.com/weather/forecast/by-lat-lng?lat='+ encodeURI(lat) + '&lng=' + encodeURI(lng) + '&filter=hourly&units=si&x-api-key=c1fc8134031f69982faef7f4c20749aec4fb98cdc3877d09aa0167c39da6917b'
     request({url, json: true}, (error, {body}) => {
         if(error) {
             callback('Unable to connect to location services!');
         } else if(!body.data){
             callback('Unable to find location!');
         } else {
-            callback(undefined, body.data.forecast[0].summary + "It's currently " + body.data.forecast[0].apparentTemperature + " degrees out. There is " + body.data.forecast[0].precipProbability + "% chance of rain.");
+            callback(undefined, "It's currently " + body.data.forecast[0].apparentTemperature + " degrees out. There is " + body.data.forecast[0].precipProbability + "% chance of rain.");
         }
     })
 };
